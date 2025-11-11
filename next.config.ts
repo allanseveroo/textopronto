c203b820a219ca9944b8f8c7d0a0d9c67519a904
Allan Severo <grupoteaser@gmail.com>
2025-11-11 22:52:10 +0000

    Error: Error fetching profile: {}     at createConsoleError (https://600

--- next.config.ts
+++ next.config.ts
@@ -30,10 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: 'https://eygvgoqtpwjjxmpnxbmc.supabase.co',
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5Z3Znb3F0cHdqanhtcG54Ym1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NjcyMDYsImV4cCI6MjA3ODQ0MzIwNn0.p_jhRGO0QsCIbs3ByuyucBgB_LyeVrim913tALA',
-  }
 };
 
 export default nextConfig;
